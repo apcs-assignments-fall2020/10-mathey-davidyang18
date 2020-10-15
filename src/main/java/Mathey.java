281545ecadf60a1b0468d351140342a267700237
davidyang18 <2817822828@qq.com>
2020-10-15 18:39:04 +0000

    In-class max methods

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -1,3 +1,37 @@
 public class Mathey {
-
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    public static double max(double a, double b) {
+        if (a > b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    public static float max(float a, float b) {
+        if (a > b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    public static int randomInteger(int a, int b) {
+        if (a < b) {
+            int num = (int) (Math.random() * (b - a) + a);
+            return num;
+        }
+        return b;
+    }
+    public static int randomInteger(int a) {
+        int num = (int) (Math.random() * (a - 0));
+        return num;
+    }
 }
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class MyMain {
 
     public static int someMethod() {
-        return 1;
+        int [] my_num = new int[3];
+        for (int i = 0; i < my_num.length; i++) {
+            int num = (int) (Math.random() * (20 - 10 + 1) + 10);
+            return num;
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.print(someMethod());
     }
 
 }
